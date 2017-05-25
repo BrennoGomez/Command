cb9fb51992888ccaf9f716e3ee3dd43a9ca77655
Brenno Gomez <brennogomez9@gmail.com>
2017-05-25 18:02:38 -0300

    Padrões de Projeto - Command

---
+++ src/ExemploWikipedia/Command.java
@@ -0,0 +1,6 @@
+package ExemploWikipedia;
+
+/** Interface do Command */
+public interface Command {
+   void execute();
+}
---
+++ src/ExemploWikipedia/Light.java
@@ -0,0 +1,13 @@
+package ExemploWikipedia;
+
+/** Classe recebedora */
+public class Light {
+
+   public void turnOn() {
+      System.out.println("A luz está ligada");
+   }
+
+   public void turnOff() {
+      System.out.println("A luz está desligada");
+   }
+}
